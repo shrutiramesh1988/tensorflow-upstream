58f96c08e561c1ad1280c5264ff2c769cad8f7b3
Eugene Kuznetsov <eugene.kuznetsov@amd.com>
2019-12-28 20:47:17 -0800

    Disable TF2 unsupported operation in unit tests

--- tensorflow/python/keras/layers/lstm_v2_test.py
+++ tensorflow/python/keras/layers/lstm_v2_test.py
@@ -317,6 +317,9 @@ class LSTMV2Test(keras_parameterized.TestCase):
 
   @test_util.run_v2_only
   def test_lstm_v2_feature_parity_with_canonical_lstm(self):
+    if build_info.is_rocm_build:
+      self.skipTest("Skipping the test as ROCm MIOpen does not support padded input yet.")
+
     input_shape = 10
     rnn_state_size = 8
     timestep = 4
@@ -756,6 +759,9 @@ class LSTMV2Test(keras_parameterized.TestCase):
 
   @test_util.run_v2_only
   def test_explicit_device_with_go_backward_and_mask(self):
+    if build_info.is_rocm_build:
+      self.skipTest("Skipping the test as ROCm MIOpen does not support padded input yet.")
+
     batch_size = 8
     timestep = 7
     masksteps = 5
@@ -854,6 +860,9 @@ class LSTMGraphRewriteTest(keras_parameterized.TestCase):
 
   @test_util.run_v2_only
   def test_LSTM_runtime_with_mask(self):
+    if build_info.is_rocm_build:
+      self.skipTest("Skipping the test as ROCm MIOpen does not support padded input yet.")
+
     # Masking will affect which backend is selected based on whether the mask
     # is strictly right padded.
     layer = rnn.LSTM(self.rnn_state_size, return_runtime=True)
